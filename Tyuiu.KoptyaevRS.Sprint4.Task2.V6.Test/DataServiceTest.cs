b78336b1b38a712749e68539b8f45de4c01e09a9
Tyuiu.KoptyaevRS@outlook.com <rustam.koptyaev@mail.ru>
2023-11-04 19:24:08 +0500

    Task2

---
+++ Tyuiu.KoptyaevRS.Sprint4.Task2.V6.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KoptyaevRS.Sprint4.Task2.V6.Lib;
+
+namespace Tyuiu.KoptyaevRS.Sprint4.Task2.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 4, 2, 6, 7, 5, 4, 3 };
+
+            int res = ds.Calculate(numsArray);
+            int wait = 105;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KoptyaevRS.Sprint4.Task2.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KoptyaevRS.Sprint4.Task2.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KoptyaevRS.Sprint4.Task2.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("26335059-66c6-452a-9375-d57791da9a20")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
